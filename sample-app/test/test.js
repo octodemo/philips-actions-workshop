21bb8ca903500591492f3da2c71818c2b2fde71f
Pedro Lacerda <pedrolacerda@github.com>
2024-09-06 00:37:49 -0300

    fix tests

--- sample-app/test/test.js
+++ sample-app/test/test.js
@@ -1,18 +1,20 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('../index');
+(async () => {
+  const chai = require('chai');
+  const chaiHttp = require('chai-http');
+  const app = require('../index');
 
-chai.use(chaiHttp);
-chai.should();
+  chai.use(chaiHttp);
+  chai.should();
 
-describe('GET /', () => {
-  it('should return 200 and a welcome message', (done) => {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.text.should.equal('Hello, GitHub Actions Workshop!');
-        done();
-      });
+  describe('GET /', () => {
+    it('should return 200 and a welcome message', (done) => {
+      chai.request(app)
+        .get('/')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.text.should.equal('Hello, GitHub Actions Workshop!');
+          done();
+        });
+    });
   });
-});
+})();
